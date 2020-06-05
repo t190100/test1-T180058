ab7c2867026317360954fe3bf00000b117ba5a94
T180058 <t180058@mail.ryukoku.ac.jp>
2020-06-05 01:54:24 +0000

    提出

--- test2.c
+++ test2.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    int i,n,a=3;
 
+    while (1) {
+        while ( i<INFINITY) {
+            i=0;
+            if (i%2==0) {
+                printf("%d\n",a);
+                a=a/2;
+            }
+            else {
+                printf("%d\n",a);
+                a=3*a+1;
+
+            }
+        if(a==1)
+            break;
+
+            i=i+1;
+        }
+    }
+}
