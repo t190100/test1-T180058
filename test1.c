4ce8f5350e7c091d8ad52ab13fbba3cc0c6b9025
T180058 <t180058@mail.ryukoku.ac.jp>
2020-06-05 01:40:45 +0000

    提出

--- test1.c
+++ test1.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+    int main(){
+        int a,n=1;
 
+        while (a<2147483647) {
+            a=n^n;
+            printf("%d\n",a);
+            n=n+1;
+
+        }
+
+        return 0;
+    }
